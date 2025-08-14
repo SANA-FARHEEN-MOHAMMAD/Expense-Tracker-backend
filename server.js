5302c67bded91d00b2a440f4c325e09e32116443
SANA-FARHEEN-MOHAMMAD <168446277+SANA-FARHEEN-MOHAMMAD@users.noreply.github.com>
2025-08-14 13:24:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,5 +38,9 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
