57d0481e7eb925370e88fe7398f3eb4657dc2c67
SANA-FARHEEN-MOHAMMAD <168446277+SANA-FARHEEN-MOHAMMAD@users.noreply.github.com>
2025-08-14 13:34:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,11 +38,12 @@ app.get("/", (req, res) => {
 });
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
